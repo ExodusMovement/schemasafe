2ab7d48e2a941b1c7175e24e1014041b4f6c4dbc
Сковорода Никита Андреевич <chalkerx@gmail.com>
2020-05-27 06:00:48 +0300

    Test everything with makeModule, too (#14)

---
+++ test-module.js
@@ -0,0 +1,22 @@
+const path = require('path')
+const validator = require('.')
+
+// This is a bit evil, but used only in tests to avoid code duplication
+// and polluting the main index.js file
+
+const id = path.join(__dirname, 'index.js')
+const indexModule = require.cache[id] // extract index.js module to override it
+
+// Sanity check
+if (validator !== indexModule.exports) throw new Error('Unexpected!')
+
+indexModule.exports = function(...args) {
+  const validate = validator(...args)
+  // eslint-disable-next-line no-new-func
+  const wrapped = new Function(`return ${validate.toModule()}`)()
+  wrapped.toModule = (...args) => validate.toModule(...args)
+  wrapped.toJSON = (...args) => validate.toJSON(...args)
+  return wrapped
+}
+
+indexModule.exports.filter = validator.filter // does not have module API
