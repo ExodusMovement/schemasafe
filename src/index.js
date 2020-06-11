3caba881b5927ac752fc88ea8e4a92babe64455e
Сковорода Никита Андреевич <chalkerx@gmail.com>
2020-06-11 22:21:31 +0300

    Only assign to module.exports once (#8)
    This simplifies bundling and removes unneeded require wrapper in
    bundled code.

--- src/index.js
+++ src/index.js
@@ -767,5 +767,5 @@ const parser = function(schema, opts = {}) {
   return parse
 }
 
+Object.assign(validator, { validator, parser })
 module.exports = validator
-Object.assign(module.exports, { validator, parser })
