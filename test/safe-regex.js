e42f62eede608ab8b389e01069288279926ef96c
Сковорода Никита Андреевич <chalkerx@gmail.com>
2020-06-12 07:15:58 +0300

    Test that all top-level format regex are full

--- test/safe-regex.js
+++ test/safe-regex.js
@@ -7,8 +7,11 @@ tape('safe-regex', function(t) {
   for (const type of Object.keys(formats)) {
     let key
     for (key of Object.keys(formats[type])) {
-      if (formats[type][key] instanceof RegExp) {
-        t.ok(safeRegex(formats[type][key]), `${key} should be a safe regex (in ${type})`)
+      const format = formats[type][key]
+      if (format instanceof RegExp) {
+        t.ok(safeRegex(format), `${key} should be a safe regex (in ${type})`)
+        t.ok(format.source.startsWith('^'), `${key} should start with ^ (in ${type})`)
+        t.ok(format.source.endsWith('$'), `${key} should end with $ (in ${type})`)
       }
     }
   }
