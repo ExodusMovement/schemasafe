afcc542c00a0e3a8f603d34d9d4a3a8f255b0e5b
Сковорода Никита Андреевич <chalkerx@gmail.com>
2020-06-18 17:51:37 +0300

    Post-process JSON.stringify to escape u2028 and u2029
    In pre-ES2019, those are not suitable for inserting in JS context.
    
    This should affect Node.js 8 and below.
    
    Refs: https://v8.dev/features/subsume-json

--- src/safe-format.js
+++ src/safe-format.js
@@ -3,6 +3,14 @@
 class SafeString extends String {} // used for instanceof checks
 
 const compares = new Set(['<', '>', '<=', '>='])
+const escapeCode = (code) => `\\u${code.toString(16).padStart(4, '0')}`
+
+// Supports simple js vaiables only, i.e. constants and JSON-stringifiable
+const jsval = (val) => {
+  if ([Infinity, -Infinity, NaN, undefined].includes(val)) return `${val}`
+  // https://v8.dev/features/subsume-json#security, e.g. {'\u2028':0} on Node.js 8
+  return JSON.stringify(val).replace(/[\u2028\u2029]/g, (char) => escapeCode(char.charCodeAt(0)))
+}
 
 const format = (fmt, ...args) => {
   const res = fmt.replace(/%[%dscj]/g, (match) => {
@@ -20,8 +28,7 @@ const format = (fmt, ...args) => {
         if (compares.has(val)) return val
         throw new Error('Expected a compare op')
       case '%j':
-        if ([Infinity, -Infinity, NaN, undefined].includes(val)) return `${val}`
-        return JSON.stringify(val)
+        return jsval(val)
     }
     /* c8 ignore next */
     throw new Error(`Unreachable`)
