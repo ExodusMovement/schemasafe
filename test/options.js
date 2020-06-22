520726c06ca894a6b25a7f59d4689a3ec92c968f
Сковорода Никита Андреевич <chalkerx@gmail.com>
2020-06-23 01:31:09 +0300

    Implement removeAdditonal (#58)
    * Implement removeAdditonal
    
    Supports only additionalProperties: false and additionalItems: false.
    
    Closes: https://github.com/ExodusMovement/schemasafe/issues/55
    
    * Add tests for removeAdditonal

--- test/options.js
+++ test/options.js
@@ -14,3 +14,43 @@ tape('dryRun', (t) => {
   t.strictEqual(typeof validator({}, { dryRun: true }), 'undefined', 'dryRun returns undefined')
   t.end()
 })
+
+tape('removeAdditional', (t) => {
+  const run = (schema, options, value) => [validator(schema, options)(value), value]
+
+  t.deepEqual(run({}, {}, { x: 1 }), [true, { x: 1 }], 'object: no rules')
+  t.deepEqual(
+    run({}, { removeAdditional: true }, { x: 1 }),
+    [true, { x: 1 }],
+    'object: lone removeAdditional'
+  )
+  t.deepEqual(
+    run({ additionalProperties: false }, {}, { x: 1 }),
+    [false, { x: 1 }],
+    'object: lone additionalProperties'
+  )
+  t.deepEqual(
+    run({ additionalProperties: false }, { removeAdditional: true }, { x: 1 }),
+    [true, {}],
+    'object: additionalProperties + removeAdditional'
+  )
+
+  t.deepEqual(run({}, {}, [1, 2]), [true, [1, 2]], 'array: no rules')
+  t.deepEqual(
+    run({}, { removeAdditional: true }, [1, 2]),
+    [true, [1, 2]],
+    'array: lone removeAdditional'
+  )
+  t.deepEqual(
+    run({ items: [true], additionalItems: false }, {}, [1, 2]),
+    [false, [1, 2]],
+    'array: items + additionalItems'
+  )
+  t.deepEqual(
+    run({ items: [true], additionalItems: false }, { removeAdditional: true }, [1, 2]),
+    [true, [1]],
+    'array: items + additionalItems + removeAdditional'
+  )
+
+  t.end()
+})
