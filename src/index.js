70272bbc55a6fb38d96654cce63f7732a5c62015
Сковорода Никита Андреевич <chalkerx@gmail.com>
2020-06-23 23:08:58 +0300

    In json-only mode, don't check object keys for existance (#50)
    * Ensure that jsonCheck is performed only once, at top level
    
    Note that it still comes with a 10x slowdown.
    It's recommended to use parser API instead.
    
    Fixes: https://github.com/ExodusMovement/schemasafe/issues/49
    
    * In json-only mode, don't check object keys for existance
    
    Object.keys are already present on the object itself,
    and can't be undefined because of JSON.
    
    Also skip hasOwn check for Object.keys in non-JSON mode,
    leave only undefined check.
    
    * fix isJSON / jsonCheck relation

--- src/index.js
+++ src/index.js
@@ -49,8 +49,8 @@ const schemaVersions = [
 const noopRegExps = new Set(['^[\\s\\S]*$', '^[\\S\\s]*$', '^[^]*$', '', '.*'])
 
 // Helper methods for semi-structured paths
-const propvar = (name, key) => ({ parent: name, keyname: key }) // property by variable
-const propimm = (name, val) => ({ parent: name, keyval: val }) // property by immediate value
+const propvar = (parent, keyname, inKeys = false) => ({ parent, keyname, inKeys }) // property by variable
+const propimm = (parent, keyval) => ({ parent, keyval }) // property by immediate value
 const buildName = ({ name, parent, keyval, keyname }) => {
   if (name) {
     if (parent || keyval || keyname) throw new Error('name can be used only stand-alone')
@@ -82,6 +82,7 @@ const compile = (schema, root, opts, scope, basePathRoot) => {
     requireValidation = opts.mode === 'strong',
     requireStringValidation = opts.mode === 'strong',
     complexityChecks = opts.mode === 'strong',
+    isJSON: optIsJSON = false, // assume input to be JSON, which e.g. makes undefined impossible
     jsonCheck = false, // disabled by default, it's assumed that data is from JSON.parse
     $schemaDefault = null,
     formats: optFormats = {},
@@ -104,6 +105,9 @@ const compile = (schema, root, opts, scope, basePathRoot) => {
     throw new Error('Strong mode demands require(String)Validation and complexityChecks')
   if (mode === 'strong' && (weakFormats || allowUnusedKeywords))
     throw new Error('Strong mode forbids weakFormats and allowUnusedKeywords')
+  if (optIsJSON && jsonCheck)
+    throw new Error('Can not specify both isJSON and jsonCheck options, please choose one')
+  const isJSON = optIsJSON || jsonCheck
 
   if (!scope[scopeCache])
     scope[scopeCache] = { sym: new Map(), ref: new Map(), format: new Map(), pattern: new Map() }
@@ -133,7 +137,12 @@ const compile = (schema, root, opts, scope, basePathRoot) => {
 
   const present = (location) => {
     const name = buildName(location) // also checks for sanity, do not remove
-    const { parent, keyval, keyname } = location
+    const { parent, keyval, keyname, inKeys } = location
+    if (inKeys) {
+      /* c8 ignore next */
+      if (isJSON) throw new Error('Unreachable: useless check, can not be undefined')
+      return format('%s !== undefined', name)
+    }
     if (parent && keyname) {
       scope.hasOwn = functions.hasOwn
       return format('%s !== undefined && hasOwn(%s, %s)', name, parent, keyname)
@@ -156,8 +165,9 @@ const compile = (schema, root, opts, scope, basePathRoot) => {
   const getMeta = () => rootMeta.get(root) || {}
   const basePathStack = basePathRoot ? [basePathRoot] : []
   const visit = (allErrors, includeErrors, history, current, node, schemaPath) => {
-    // e.g. top-level data and property names, OR already checked by present() in history
-    const definitelyPresent = !current.parent || history.includes(current)
+    // e.g. top-level data and property names, OR already checked by present() in history, OR in keys and not undefined
+    const definitelyPresent =
+      !current.parent || history.includes(current) || (current.inKeys && isJSON)
 
     const name = buildName(current)
     const writeErrorObject = (error) => {
@@ -331,7 +341,7 @@ const compile = (schema, root, opts, scope, basePathRoot) => {
           cache.ref.set(sub, n)
           let fn = null // resolve cyclic dependencies
           scope[n] = (...args) => fn(...args)
-          const override = { includeErrors: false, jsonCheck: false }
+          const override = { includeErrors: false, jsonCheck: false, isJSON }
           fn = compile(sub, subRoot, { ...opts, ...override }, scope, path)
           scope[n] = fn
         }
@@ -647,7 +657,8 @@ const compile = (schema, root, opts, scope, basePathRoot) => {
           for (const p of Object.keys(node.patternProperties)) {
             enforceRegex(p, node.propertyNames || {})
             fun.block('if (%s.test(%s)) {', [patterns(p), key], '}', () => {
-              rule(propvar(name, key), node.patternProperties[p], subPath('patternProperties', p))
+              const sub = propvar(name, key, true) // always own property, from Object.keys
+              rule(sub, node.patternProperties[p], subPath('patternProperties', p))
             })
           }
         })
@@ -671,7 +682,8 @@ const compile = (schema, root, opts, scope, basePathRoot) => {
                 error('has additional properties', null, format('%j + %s', `${name}.`, key))
               }
             } else {
-              rule(propvar(name, key), node.additionalProperties, subPath('additionalProperties'))
+              const sub = propvar(name, key, true) // always own property, from Object.keys
+              rule(sub, node.additionalProperties, subPath('additionalProperties'))
             }
           })
         })
@@ -820,7 +832,7 @@ const validator = (schema, opts = {}) => compile(schema, schema, opts, Object.cr
 
 const parser = function(schema, opts = {}) {
   // strong mode is default in parser
-  const validate = validator(schema, { mode: 'strong', ...opts })
+  const validate = validator(schema, { mode: 'strong', ...opts, jsonCheck: false, isJSON: true })
   const parse = (src) => {
     if (typeof src !== 'string') throw new Error('Invalid type!')
     const data = JSON.parse(src)
