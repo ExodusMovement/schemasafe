9ba2ed69c6b8dd362c8ca899fe4a4b3c2fabab8a
Сковорода Никита Андреевич <chalkerx@gmail.com>
2020-07-10 10:16:52 +0300

    Expand schema versions from draft versions

--- src/known-keywords.js
+++ src/known-keywords.js
@@ -18,14 +18,9 @@ const knownKeywords = [
   ...['description', 'title', 'examples', '$comment'], // unused
 ]
 
-// Order is important, newer at the top!
-const schemaVersions = [
-  'https://json-schema.org/draft/2019-09/schema',
-  'https://json-schema.org/draft-07/schema',
-  'https://json-schema.org/draft-06/schema',
-  'https://json-schema.org/draft-04/schema',
-  'https://json-schema.org/draft-03/schema',
-]
+// Order is important, newer first!
+const schemaDrafts = ['draft/2019-09', 'draft-07', 'draft-06', 'draft-04', 'draft-03']
+const schemaVersions = schemaDrafts.map((draft) => `https://json-schema.org/${draft}/schema`)
 
 const vocab2019 = ['core', 'applicator', 'validation', 'meta-data', 'format', 'content']
 const knownVocabularies = vocab2019.map((v) => `https://json-schema.org/draft/2019-09/vocab/${v}`)
