f46e742d8cb38474c44aef7ce53e94ba46ee7762
Сковорода Никита Андреевич <chalkerx@gmail.com>
2020-07-22 13:11:52 +0300

    test: direct tests for date-time format

--- test/formats.js
+++ test/formats.js
@@ -1,19 +1,43 @@
 'use strict'
 
 const tape = require('tape')
-const { date } = require('../src/formats').core
+const { core } = require('../src/formats')
+
+const badDates = ['2019-02-29', '2019-02-30', '2019-02-31', '2020-02-30', '2020-02-31']
 
 tape('date', (t) => {
-  for (const d of ['2019-02-29', '2019-02-30', '2019-02-31', '2020-02-30', '2020-02-31'])
-    t.strictEqual(date(d), false, 'Date fails on invalid date')
+  for (const d of badDates) t.strictEqual(core.date(d), false, 'date fails on invalid date')
 
+  const check = (val) => {
+    if (!core.date(val)) throw new Error(`False negative: ${val}`)
+  }
   t.doesNotThrow(() => {
     const now = Date.now()
     for (let i = 0; i < 1e5; i++) {
       const string = new Date(2 * now * Math.random()).toISOString().slice(0, 10)
-      if (!date(string)) throw new Error('False negative!')
+      check(string)
     }
-  }, 'Date passes on valid dates')
+  }, 'date passes on valid dates')
+
+  t.end()
+})
+
+tape('date-time', (t) => {
+  for (const d of badDates)
+    t.strictEqual(core['date-time'](`${d}T12:00:00Z`), false, 'date-time fails on invalid date')
+
+  const check = (val) => {
+    if (!core['date-time'](val)) throw new Error(`False negative: ${val}`)
+  }
+  t.doesNotThrow(() => {
+    const now = Date.now()
+    for (let i = 0; i < 1e5; i++) {
+      const string = new Date(2 * now * Math.random()).toISOString()
+      check(string)
+      check(`${string.slice(0, 19)}Z`)
+      check(`${string.slice(0, 22)}Z`)
+    }
+  }, 'date-time passes on valid date-time values')
 
   t.end()
 })
