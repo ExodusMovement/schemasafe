08f9ba8c77007938c04dea58c9b9bee1ca56d3d0
Сковорода Никита Андреевич <chalkerx@gmail.com>
2020-10-08 02:57:00 +0300

    fix: use actual options set in types

--- index.d.ts
+++ index.d.ts
@@ -9,9 +9,26 @@ interface Validate {
 }
 
 interface ValidatorOptions {
-  allErrors?: boolean;
-  extraFormats?: boolean;
+  mode?: string,
+  useDefaults?: boolean;
+  removeAdditional?: boolean;
   includeErrors?: boolean;
+  allErrors?: boolean;
+  dryRun?: boolean;
+  allowUnusedKeywords?: boolean;
+  allowUnreachable?: boolean;
+  requireSchema?: boolean;
+  requireValidation?: boolean;
+  requireStringValidation?: boolean;
+  forbidNoopValues?: boolean;
+  complexityChecks?: boolean;
+  unmodifiedPrototypes?: boolean;
+  isJSON?: boolean;
+  $schemaDefault?: string | null;
+  formats?: any; // FIXME
+  weakFormats?: boolean;
+  extraFormats?: boolean;
+  schemas?: any; // FIXME
 }
 
 declare const validator: (schema: object, options?: ValidatorOptions) => Validate;
