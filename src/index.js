26cc63683b1b722d42a2036e8fa5ea9a2fd271c1
Nikita Skovoroda <chalkerx@gmail.com>
2022-10-09 11:06:35 +0400

    cleanup: split some code out of parser()/validator()
    Preparation for multi-schema version.

--- src/index.js
+++ src/index.js
@@ -5,6 +5,20 @@ const { buildSchemas } = require('./pointer')
 const { compile } = require('./compile')
 const functions = require('./scope-functions')
 
+function writeJsonCheck(fun, validate, includeErrors) {
+  if (includeErrors) {
+    fun.write('if (!deepEqual(data, JSON.parse(JSON.stringify(data)))) {')
+    fun.write('validateIsJSON.errors = [{instanceLocation:"#",error:"not JSON compatible"}]')
+    fun.write('return false')
+    fun.write('}')
+    fun.write('const res = %s(data)', validate)
+    fun.write('validateIsJSON.errors = actualValidate.errors')
+    fun.write('return res')
+  } else {
+    fun.write('return deepEqual(data, JSON.parse(JSON.stringify(data))) && %s(data)', validate)
+  }
+}
+
 const validator = (schema, { jsonCheck = false, isJSON = false, schemas = [], ...opts } = {}) => {
   if (jsonCheck && isJSON) throw new Error('Can not specify both isJSON and jsonCheck options')
   const options = { ...opts, schemas: buildSchemas(schemas, [schema]), isJSON: isJSON || jsonCheck }
@@ -14,20 +28,9 @@ const validator = (schema, { jsonCheck = false, isJSON = false, schemas = [], ..
   if (!jsonCheck) {
     fun.write('%s', refs[0])
   } else {
-    // jsonCheck wrapper implementation below
     scope.deepEqual = functions.deepEqual
     fun.write('function validateIsJSON(data) {')
-    if (opts.includeErrors) {
-      fun.write('if (!deepEqual(data, JSON.parse(JSON.stringify(data)))) {')
-      fun.write('validateIsJSON.errors = [{instanceLocation:"#",error:"not JSON compatible"}]')
-      fun.write('return false')
-      fun.write('}')
-      fun.write('const res = %s(data)', refs[0])
-      fun.write('validateIsJSON.errors = actualValidate.errors')
-      fun.write('return res')
-    } else {
-      fun.write('return deepEqual(data, JSON.parse(JSON.stringify(data))) && %s(data)', refs[0])
-    }
+    writeJsonCheck(fun, refs[0], opts.includeErrors)
     fun.write('}')
   }
   const validate = fun.makeFunction(scope)
@@ -36,45 +39,42 @@ const validator = (schema, { jsonCheck = false, isJSON = false, schemas = [], ..
   return validate
 }
 
+const parseWithErrors = (validate) => (src) => {
+  if (typeof src !== 'string') return { valid: false, error: 'Input is not a string' }
+  try {
+    const value = JSON.parse(src)
+    if (!validate(value)) {
+      const { keywordLocation, instanceLocation } = validate.errors[0]
+      const keyword = keywordLocation.slice(keywordLocation.lastIndexOf('/') + 1)
+      const error = `JSON validation failed for ${keyword} at ${instanceLocation}`
+      return { valid: false, error, errors: validate.errors }
+    }
+    return { valid: true, value }
+  } catch ({ message }) {
+    return { valid: false, error: message }
+  }
+}
+
+const parseWithoutErrors = (validate) => (src) => {
+  if (typeof src !== 'string') return { valid: false }
+  try {
+    const value = JSON.parse(src)
+    if (!validate(value)) return { valid: false }
+    return { valid: true, value }
+  } catch (e) {
+    return { valid: false }
+  }
+}
+
 const parser = function(schema, opts = {}) {
   // strong mode is default in parser
   if (functions.hasOwn(opts, 'jsonCheck') || functions.hasOwn(opts, 'isJSON'))
     throw new Error('jsonCheck and isJSON options are not applicable in parser mode')
   const validate = validator(schema, { mode: 'strong', ...opts, jsonCheck: false, isJSON: true })
-  const parse = opts.includeErrors
-    ? (src) => {
-        if (typeof src !== 'string') return { valid: false, error: 'Input is not a string' }
-        try {
-          const value = JSON.parse(src)
-          if (!validate(value)) {
-            const { keywordLocation, instanceLocation } = validate.errors[0]
-            const keyword = keywordLocation.slice(keywordLocation.lastIndexOf('/') + 1)
-            const error = `JSON validation failed for ${keyword} at ${instanceLocation}`
-            return { valid: false, error, errors: validate.errors }
-          }
-          return { valid: true, value }
-        } catch ({ message }) {
-          return { valid: false, error: message }
-        }
-      }
-    : (src) => {
-        if (typeof src !== 'string') return { valid: false }
-        try {
-          const value = JSON.parse(src)
-          if (!validate(value)) return { valid: false }
-          return { valid: true, value }
-        } catch (e) {
-          return { valid: false }
-        }
-      }
+  const parseWith = opts.includeErrors ? parseWithErrors : parseWithoutErrors
+  const parse = parseWith(validate)
   parse.toModule = ({ semi = true } = {}) =>
-    [
-      '(function(src) {',
-      `const validate = ${validate.toModule()}`,
-      `const parse = ${parse}\n`,
-      'return parse(src)',
-      `})${semi ? ';' : ''}`,
-    ].join('\n')
+    `(${parseWith})(${validate.toModule({ semi: false })})${semi ? ';' : ''}`
   parse.toJSON = () => schema
   return parse
 }
